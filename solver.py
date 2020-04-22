972c8db631b08c9666642ce76c0e03917d6c1670
BlakeC97 <cjblake97@gmail.com>
2020-04-22 12:13:40 -0400

    Rework Solver API to allow searching for mines

--- solver.py
+++ solver.py
@@ -11,7 +11,8 @@ class MineSAT:
         self.width = len(board[0])
         self.threads = num_solver_threads
 
-    def try_tile(self, row: int, col: int) -> Optional[Coordinate]:
+
+    def try_tile(self, row: int, col: int, find_safe: bool = True) -> Optional[Coordinate]:
         # Set up our "board", with space to place borders that will simplify calculations.
         # e.g. With a 9x9 board, internally we'll represent it as 11x11 with the extra rows always set to 0
         board = cp_model.CpModel()
@@ -39,21 +40,30 @@ class MineSAT:
                         + board_vars[r+1][c-1] + board_vars[r+1][c] + board_vars[r+1][c+1] == int(tile) # Down-left, down, down-right
                     )
 
-        # Place the mine
-        board.Add(board_vars[row][col] == 1)
+        if find_safe:
+            # Place the mine
+            board.Add(board_vars[row][col] == 1)
+        else:
+            # Place the "safe tile" (does that even make sense?)
+            board.Add(board_vars[row][col] == 0)
 
         solver = cp_model.CpSolver()
         solver.parameters.num_search_workers = self.threads
-        # We only want infeasible solutions, since it means a mine 100% cannot be placed in (row, col)
+        # We only want infeasible solutions, since it means the desired tile 100% cannot be placed in (row, col)
         if solver.Solve(board) == cp_model.INFEASIBLE:
             return (row, col)
 
-    def find_safe_tiles(self) -> List[Coordinate]:
-        safe_tiles = []
+
+    def find_tiles(self, tile_type: str = "safe") -> List[Coordinate]:
+        if tile_type.lower() not in {"safe", "mine"}:
+            raise ValueError(f'Tile type {tile_type} is not supported. Available types: "safe", "mine"')
+        _find_safe = True if tile_type.lower()=="safe" else False
+
+        found_tiles = []
         for r in range(self.length):
             for c in range(self.width):
                 if self.board[r][c] == "?":
-                    t = self.try_tile(r+1, c+1)
+                    t = self.try_tile(r+1, c+1, find_safe=_find_safe)
                     if t is not None:
-                        safe_tiles.append(t)
-        return safe_tiles
+                        found_tiles.append(t)
+        return found_tiles
